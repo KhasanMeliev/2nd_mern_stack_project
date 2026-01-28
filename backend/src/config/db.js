24689829daaa19f0d58d05a9136d0ee5b1dd69f5
KhasanMeliev <khasanmeliev2@gmail.com>
2026-01-28 19:49:07 +0500

    .env file fixed

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://khasanmeliev2_db_user:EOuKXO6Venkw4UuQ@cluster0.bvfeljz.mongodb.net/?appName=Cluster0",
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./router/product.route.js";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env", quiet: true });
+
 const app = express();
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use("/api/products/", productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server is running on port 5000");
+  console.log(`Server is running on port ${PORT}`);
 });
